59d2b443973a3243b9db9090a66edec22198d2cb
Graham Richards <grahamdrichards@gmail.com>
2021-11-28 08:51:37 +0000

    writeMaterial

--- ninefreq.ts
+++ ninefreq.ts
@@ -1,1 +1,23 @@
-console.log(`hello colpourful wourld`)
+import { writeFileSync } from 'fs'
+
+const writeMaterial = ( 
+  name: string, 
+  red: number, 
+  green: number,
+  blue: number,
+  dir: string,
+  ) => {
+    const output = `
+newmtl ${name}
+Ka ${red} ${green} ${blue} 
+Kd ${red} ${green} ${blue} 
+Ks ${red} ${green} ${blue}
+Ns 100
+Ni 1.000000 
+d 1.000000 
+illum 0 
+`
+    writeFileSync(`${dir}/${name}.mtl`, output)
+  }
+
+  writeMaterial("pink", 0.9, 0.8, 0.8, "output")
